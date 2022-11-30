61605a6fbdf53a2f6c1f2f30ba12fac13617fe45
Yosef Shulman <yosef.shulman@protonmail.com>
2022-11-30 16:21:01 +0200

    Refactoring of Service and Controller according to task requirements

--- Controllers/NewsController.cs
+++ Controllers/NewsController.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace LandcareResearch.Controllers;
 
 [ApiController]
-[Route("[controller]")]
+[Route("api/[controller]")]
 public class NewsController : ControllerBase
 {
     public NewsController()
@@ -13,28 +13,29 @@ public class NewsController : ControllerBase
     }
 
     [HttpGet]
-    public ActionResult<List<News>> GetAll() =>
-        NewsService.GetAll();
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<ActionResult<List<Article>>> Get() {
+        var articles = await NewsService.GetArticles();
 
-    [HttpGet("{id:int}")]
-    public ActionResult<News> Get(int id)
-    {
-        var news = NewsService.Get(id);
-
-        if(news == null)
+        if(articles == null)
             return NotFound();
 
-        return news;
+        return articles;
     }
+        
     
-    [HttpGet("find/{term}")]
-    public ActionResult<News> Get(string term)
+    [HttpGet("search/{term}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<ActionResult<List<Article>>> Search(string term)
     {
-        var news = NewsService.Search(term);
+        var articles = await NewsService.GetArticles(term);
 
-        if(news == null)
+        if(articles == null)
             return NotFound();
 
-        return news;
+        return articles;
     }
 }
